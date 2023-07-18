06dfe8d8ecc13521b93237d06a417029c5279ed2
andre059 <andreyperm159@mail.ru>
2023-07-18 15:36:40 +0500

    config/ main/ manage.py requirements.txt static/

---
+++ main/management/commands/fill.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from main.models import Student
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        students_list = [
+            {'first_name': 'Oleg', 'last_name': 'Maslov'},
+            {'first_name': 'Aleksey', 'last_name': 'Matyuk'},
+            {'first_name': 'Julia', 'last_name': 'Blohina'},
+        ]
+
+        students_objects = []
+        for students_item in students_list:
+            students_objects.append(Student(**students_item))
+
+        Student.objects.bulk_create(students_objects)
